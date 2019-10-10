aaffd68e6f5841126afc4676d37e6e030f9a4b20
Thibaut Lienart <ltib@me.com>
2019-10-11 00:31:28 +0200

    Updates

--- scripts/EX-AMES.jl
+++ scripts/EX-AMES.jl
@@ -1,3 +1,15 @@
+# Before running this, make sure to activate the environment corresponding to
+# [this `Project.toml`](https://raw.githubusercontent.com/tlienart/MLJTutorials/master/Project.toml)
+# and update it so that you get an environment which matches the one used to generate
+# the tutorials:
+#
+# ```julia
+# cd("MLJTutorials") # cd to folder with the Project.toml
+# using Pkg
+# Pkg.activate(".")
+# Pkg.update()
+# ```
+
 using MLJ, MLJBase, PrettyPrinting, DataFrames, Statistics
 MLJ.color_off() # hide / @reader: feel free to comment this out
 
